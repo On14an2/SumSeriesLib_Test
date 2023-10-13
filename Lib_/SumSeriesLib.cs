0ceef1d672d331dc8d889da68d9ae6a98df314d5
on14an <112744129+On14an2@users.noreply.github.com>
2023-10-13 14:57:49 +0700

    Добавьте файлы проекта.

---
+++ Lib_/SumSeriesLib.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lib
+{
+    public class SumSeriesLib
+    {
+        public void CalculateSumSeries(int x,double eps)
+        {
+            int devider = 1;
+            double res;
+            double sum = 0;
+            int count = 2;
+            sum = devider + x;//1 + x
+            Console.WriteLine(sum);
+            Console.WriteLine("---------------------");
+            res = x * x / (devider * count);//x^2/2!
+            Console.WriteLine(res);
+            sum += res; //1+x + x^2/2
+            Console.WriteLine(sum);
+            count++;
+            Console.WriteLine("---------------------");
+            while (eps <= res)
+            {
+                res = res * x / count;
+                count++;
+                sum += res;
+                Console.WriteLine(res);
+                Console.WriteLine(sum);
+                Console.WriteLine("---------------------");
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lib;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static SumSeriesLib calcLib = new SumSeriesLib();
+
+        private static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            double eps = Convert.ToDouble(Console.ReadLine());
+            calcLib.CalculateSumSeries(x, eps);
+        }
+    }
+}
