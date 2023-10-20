c8cc5970d4cd4d85d553c21c84960f326aa1b798
on14an <112744129+On14an2@users.noreply.github.com>
2023-10-20 15:15:26 +0700

    FInal

--- Lib_/SumSeriesLib.cs
+++ Lib_/SumSeriesLib.cs
@@ -1,33 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
 
 namespace Lib
 {
     public class SumSeriesLib
     {
-        public void CalculateSumSeries(int x,double eps)
+        public List<double> CalculateSumSeries(int x, double eps)
         {
             int devider = 1;
             double res;
             double sum = 0;
             int count = 2;
             sum = devider + x;//1 + x
-            Console.WriteLine(sum);
-            Console.WriteLine("---------------------");
+            List<double> results = new List<double> { sum };
             res = x * x / (devider * count);//x^2/2!
-            Console.WriteLine(res);
+            results.Add(res);
             sum += res; //1+x + x^2/2
-            Console.WriteLine(sum);
+            results.Add(sum);
             count++;
-            Console.WriteLine("---------------------");
             while (eps <= res)
             {
                 res = res * x / count;
                 count++;
                 sum += res;
-                Console.WriteLine(res);
-                Console.WriteLine(sum);
-                Console.WriteLine("---------------------");
+                results.Add(res);
+                results.Add(sum); 
             }
+            return results;
         }
     }
 }
---
+++ SumSerial/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib;
+
+namespace SumSerial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<double> result;
+        public MainWindow()
+        {
+            InitializeComponent();
+            result = new List<double>();
+        }
+
+        private void Calc(object sender, RoutedEventArgs e)
+        {
+            SumSeriesLib sumSeries = new SumSeriesLib();
+            int x = Convert.ToInt32(XInput.Text);
+            double eps = Convert.ToDouble(EpsInput.Text);
+            result = sumSeries.CalculateSumSeries(x, eps);
+            Results.ItemsSource = result;
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            XInput.Text = string.Empty;
+            EpsInput.Text = string.Empty;
+        }
+
+        private void WriteInFile(object sender, RoutedEventArgs e) 
+        {
+            try
+            {
+
+                string filePath = "D:\\output.txt";
+                
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePath))
+                {
+                    foreach (double item  in result)
+                    {
+                        file.WriteLine(item);
+                    }
+                }
+
+                MessageBox.Show("Данные сохранены в файле: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при сохранении данных: " + ex.Message);
+            }
+        }
+    }
+}
